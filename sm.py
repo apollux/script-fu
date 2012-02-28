bbeba4b1f79de2cfd360efee29618985d5d2fd8a
Alexandru Scvortov <scvalex@gmail.com>
2012-02-28 19:38:45 +0000

    implement most of sm's functionality

--- sm.py
+++ sm.py
@@ -5,10 +5,16 @@ from __future__ import print_function
 import cairo
 import gobject
 import gtk
+import string
 import pygtk
 
 class Screen(gtk.DrawingArea):
     __gsignals__ = { "expose-event": "override" }
+
+    def __init__(self):
+        super(Screen, self).__init__()
+
+        self.text = [[]]
 
     def do_expose_event(self, event):
         cr = self.window.cairo_create()
@@ -19,22 +25,61 @@ class Screen(gtk.DrawingArea):
 
         self.draw(cr, *self.window.get_size())
 
+    def on_key_press(self, event):
+        keyname = gtk.gdk.keyval_name(event.keyval)
+        if keyname == "Escape":
+            gtk.main_quit()
+        elif keyname == "space":
+            self.text.append([])
+        elif keyname == "Return":
+            self.text = [[]]
+        elif keyname == "BackSpace":
+            last = self.text[-1]
+            if len(last) > 0:
+                self.text[-1] = last[:-1]
+            elif len(self.text) > 1:
+                del self.text[-1]
+        elif event.keyval < 256 and self.is_printable_char(chr(event.keyval)):
+            self.text[-1].append(chr(event.keyval))
+        else:
+            print("Unhandled key '%s' pressed" % (keyname, ))
+
+        self.queue_draw()
+
     def draw(self, cr, width, height):
         cr.set_source_rgb(1.0, 1.0, 1.0)
         cr.paint()
 
-def main():
-    def on_key_press(widget, event):
-        keyname = gtk.gdk.keyval_name(event.keyval)
-        if keyname == "Escape":
-            gtk.main_quit()
-        else:
-            print("Unhandled key '%s' pressed" % (keyname, ))
+        cr.set_source_rgb(0.0, 0.0, 0.0)
+        cr.select_font_face("sans-serif")
 
+        self.show_texts(cr, width, height, 40, 200)
+
+    def is_printable_char(self, key):
+        return key in (string.digits + string.letters + string.punctuation)
+
+    def show_texts(self, cr, width, height, min_text_size, max_text_size):
+        text_size = min_text_size
+        text = " ".join(("".join(cs) for cs in self.text))
+
+        size_increment = int(max_text_size - min_text_size)
+        while size_increment > 0:
+            cr.set_font_size(text_size + size_increment)
+            (_, _, text_width, text_height, _, _) = cr.text_extents(text)
+            if text_width < width * 0.9:
+                text_size += size_increment
+            size_increment = int(size_increment / 2)
+
+        cr.set_font_size(text_size + size_increment)
+        (_, _, text_width, text_height, _, _) = cr.text_extents(text)
+        cr.move_to(width / 2 - text_width / 2, height / 2 + text_height / 2)
+        cr.show_text(text)
+
+def main():
     window = gtk.Window()
     window.connect("delete-event", gtk.main_quit)
-    window.connect("key_press_event", on_key_press)
     widget = Screen()
+    window.connect("key_press_event", lambda w, ev: widget.on_key_press(ev))
     window.add(widget)
     widget.show()
     window.present()
