6e3e324a0c784456cacda5fbe5d93d8d7446b40f
Alexandru Scvortov <scvalex@gmail.com>
2012-07-30 00:44:42 +0100

    multi-row layout for sm.py

--- sm.py
+++ sm.py
@@ -60,32 +60,75 @@ class Screen(gtk.DrawingArea):
         self.show_texts(cr, width, height, 40, 200)
 
     def show_texts(self, cr, width, height, min_text_size, max_text_size):
-        def fit_text(num_rows):
-            return (num_rows * 10, 40)
+        line_spacing = 1.6
+
+        def text_dims(text):
+            (_, _, text_width, text_height, _, _) = cr.text_extents(text)
+            return (text_width, text_height)
+
+        def pack_words(ws, font_size, twidth):
+            rows = []
+            i = 0
+            badness = 0.0
+            cr.set_font_size(font_size)
+            (a_width, a_height) = text_dims("a")
+            l = len(ws)
+            sum_h = 0.0
+            while i < l:
+                (cwidth, _) = text_dims(ws[i])
+                row = [ws[i]]
+                i += 1
+                while i < l:
+                    (nwidth, h) = text_dims(ws[i])
+                    if cwidth + nwidth + a_width <= twidth:
+                        cwidth += nwidth + a_width
+                        row.append(ws[i])
+                        i += 1
+                    else:
+                        break
+                # print("twidth = %f; cwidth = %f" % (twidth, cwidth))
+                badness += max(0.0, (abs(twidth - cwidth) - twidth * 1.0 / 6.0))
+                sum_h += a_height * line_spacing
+                rows.append(row)
+            if sum_h > height * 5.0 / 6.0:
+                badness = float("inf")
+            return (badness, rows)
+
+        words = ["".join(cs) for cs in self.text]
+
+        twidth = 2.0 / 3.0 * width
 
         text_size = min_text_size
-        text = " ".join(("".join(cs) for cs in self.text))
-
-        best_badness, best_text_size, best_num_rows = float('inf'), 0, 0
-        for num_rows in xrange(1, len(self.text) + 1):
-            (badness, text_size) = fit_text(num_rows)
-            print("Trying %d rows; badness = %f" % (num_rows, badness))
-            if badness < best_badness:
-                best_badness, best_text_size = badness, text_size
-                best_num_rows = num_rows
-
+        # print("initial text_size = %d" % (text_size,))
         size_increment = int(max_text_size - min_text_size)
+        (best_badness, best_rows) = pack_words(words, text_size, twidth)
+        best_badness += 1.6 ** (max_text_size - text_size)
         while size_increment > 0:
-            cr.set_font_size(text_size + size_increment)
-            (_, _, text_width, text_height, _, _) = cr.text_extents(text)
-            if text_width < width * 0.9:
+            # print("computing for size_increment = %d" % (size_increment, ))
+            (badness, rows) = pack_words(words, text_size + size_increment, twidth)
+            badness += 1.6 ** (max_text_size - (text_size + size_increment))
+            # print("badness = %f" % (badness,))
+            if badness < best_badness:
                 text_size += size_increment
+                # print("text_size = %d" % (text_size, ))
+                best_rows = rows
+                best_badness = badness
             size_increment = int(size_increment / 2)
 
+        # print("displaying text: %d" % (text_size, ))
+
         cr.set_font_size(text_size)
-        (_, _, text_width, text_height, _, _) = cr.text_extents(text)
-        cr.move_to(width / 2 - text_width / 2, height / 2 + text_height / 2)
-        cr.show_text(text)
+        (a_width, a_height) = text_dims("a")
+        y = a_height * line_spacing
+        # print("best_rows = %s" % (str(best_rows), ))
+        for row in best_rows:
+            x = width * 1.0 / 6.0
+            for w in row:
+                cr.move_to(x, y)
+                cr.show_text(w)
+                (w, _) = text_dims(w)
+                x += w + a_width
+            y += a_height * line_spacing
 
 def main():
     window = gtk.Window()
